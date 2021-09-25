b13f51e7c64502f8b17bde596429f3a898edd0db
Brtgkrk <jakubkrok2000@interia.pl>
2021-09-25 14:14:25 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WSEI_Programowanie_projekt_1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
